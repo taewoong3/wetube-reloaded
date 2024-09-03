ccb09c8fff928f0996a6ac0f5a4b06ae3a0a6bc4
taewoong3 <https://github.com/taewoong3>
2024-09-04 01:00:23 +0900

    2024-09-04(수)
    Video Play - Mute Settings

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,57 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Paused";
+};
+
+const handleMute = (e) => {
+  // 처음에는 음소거가 되지 않았기 때문에 false
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+
+  if (value == 0) {
+    muteBtn.innerText = "UnMute";
+  } else {
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange); //change 이벤트는 마우스를 놓았을 때, input 이벤트는 실시간
+
+/**
+ * 이러한 이벤트도 있다는걸 알려주는 용도
+ */
+// video.addEventListener("pause", handlePause);
+// video.addEventListener("play", handlePlay);
