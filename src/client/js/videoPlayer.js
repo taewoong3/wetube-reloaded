c2259634debeddda5ced561c0e0018fba1b29f36
taewoong3 <https://github.com/taewoong3>
2024-09-05 00:13:33 +0900

    2024-09-05 (Thu) Update
    Video.duration / Video.currentTime

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,6 +3,8 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -14,6 +16,7 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+
   playBtn.innerText = video.paused ? "Play" : "Paused";
 };
 
@@ -46,9 +49,27 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring(14, 19);
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration)); //<video> 요소의 총 재생 시간을 초 단위로 나타내는 속성
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime)); //현재 video가 실행되는 시간을 말함.
+
+  if (video.currentTime == video.duration) {
+    playBtn.innerText = "Play";
+  } else {
+    playBtn.innerText = "Paused";
+  }
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange); //change 이벤트는 마우스를 놓았을 때, input 이벤트는 실시간
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
 
 /**
  * 이러한 이벤트도 있다는걸 알려주는 용도
