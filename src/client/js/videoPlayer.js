feeb6fc52fc754b21300cb7c67427d01c2a45fe3
taewoong3 <https://github.com/taewoong3>
2024-09-06 00:01:11 +0900

    2024-09-06(Fri) Update
    TimeLine - FullScreen - setTimeout

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,7 +5,12 @@ const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoController");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -53,10 +58,12 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring
 
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration)); //<video> 요소의 총 재생 시간을 초 단위로 나타내는 속성
+  timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime)); //현재 video가 실행되는 시간을 말함.
+  timeline.value = Math.floor(video.currentTime);
 
   if (video.currentTime == video.duration) {
     playBtn.innerText = "Play";
@@ -65,11 +72,46 @@ const handleTimeUpdate = () => {
   }
 };
 
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+  const fullScreen = document.fullscreenElement;
+  if (fullScreen != null) {
+    document.exitFullscreen();
+  } else {
+    videoContainer.requestFullscreen();
+  }
+  fullScreenBtn.innerText = fullScreen == null ? "Exit Full Screen" : "Enter Full Screen";
+};
+
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  videoControls.classList.add("showing");
+};
+
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(() => {
+    videoControls.classList.remove("showing");
+  }, 3000);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange); //change 이벤트는 마우스를 놓았을 때, input 이벤트는 실시간
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
-video.addEventListener("timeupdate", handleTimeUpdate);
+video.addEventListener("timeupdate", handleTimeUpdate); // 비디오 시간이 변경되는걸 감지하는 event.
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
 
 /**
  * 이러한 이벤트도 있다는걸 알려주는 용도
