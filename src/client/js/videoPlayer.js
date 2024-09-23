d96d6c4d25bbea7073c1d1663b9fcceaec3a3656
taewoong3 <https://github.com/taewoong3>
2024-09-24 03:52:21 +0900

    24-09-24 Update
    new Update

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,15 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoController");
 const videoControls = document.getElementById("videoControls");
 
@@ -22,7 +25,7 @@ const handlePlayClick = () => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Paused";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMute = () => {
@@ -32,7 +35,7 @@ const handleMute = () => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  muteBtnIcon.classList = video.muted ? "fas fa-volume-mute" : "fas fa-volume-up";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -54,11 +57,13 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+// const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring(14, 19);
 const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substring(14, 19);
 
+// UI 업데이트
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration)); //<video> 요소의 총 재생 시간을 초 단위로 나타내는 속성
-  timeline.max = Math.floor(video.duration);
+  timeline.max = Math.floor(video.duration); // 오직 메타데이터로 로드 되었을 때만, 비디오의 길이를 알 수 있다.
 };
 
 const handleTimeUpdate = () => {
@@ -79,12 +84,13 @@ const handleTimelineChange = (event) => {
 
 const handleFullScreen = () => {
   const fullScreen = document.fullscreenElement;
-  if (fullScreen != null) {
+  if (fullScreen) {
     document.exitFullscreen();
+    fullScreenIcon.classList = "fas fa-expand";
   } else {
     videoContainer.requestFullscreen();
+    fullScreenIcon.classList = "fas fa-compress";
   }
-  fullScreenBtn.innerText = fullScreen == null ? "Exit Full Screen" : "Enter Full Screen";
 };
 
 const hideControls = () => videoControls.classList.remove("showing");
@@ -99,7 +105,7 @@ const handleMouseMove = () => {
     controlsMovementTimeout = null;
   }
   videoControls.classList.add("showing");
-  controlsMovementTimeout = setTimeout(hideControls, 3000);
+  controlsMovementTimeout = setTimeout(hideControls, 3000); // Timeout을 생성할 때 고유한ID 를 반환한다.
 };
 
 const handleMouseLeave = () => {
@@ -109,10 +115,11 @@ const handleMouseLeave = () => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange); //change 이벤트는 마우스를 놓았을 때, input 이벤트는 실시간
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+//video.addEventListener("loadedmetadata", handleLoadedMetadata); // 예: 비디오의 너비, 높이, 지속 시간 등 해당 데이터를 메타데이터라 하고, 해당 데이터로 로드 되었을 때 발생하는 이벤트
+video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate); // 비디오 시간이 변경되는걸 감지하는 event.
-video.addEventListener("mousemove", handleMouseMove);
-video.addEventListener("mouseleave", handleMouseLeave);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
 
